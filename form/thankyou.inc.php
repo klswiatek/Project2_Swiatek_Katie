551989e17cb3d7d75a4d198e4a5b524d1ea63247
Katie Lynn Swiatek <klswiatek@yahoo.com>
2021-11-09 13:43:56 -0500

    Contact and Work Sample pages added from Web 1 first version site

---
+++ form/thankyou.inc.php
@@ -0,0 +1,81 @@
+<style type="text/css">
+* { margin: 0;
+padding: 0;
+border: 0;
+}
+body {
+  background-image: url('../image/marble.jpeg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+}
+
+p {
+  display: block;
+  padding: 7px;
+  font-size: 20px;
+  text-align: center;
+  font-family: 'open sans';
+}
+
+p::first-line {
+    color: #800020;
+  }
+
+nav {
+    background-color: #808080;
+    text-align: left;
+    color: white;
+    font-family: sans-serif;
+    font-size: 20px;
+    padding: 15px;
+    width: 100%;
+    font-family: 'Open Sans', sans-serif;
+  }
+
+h3 {
+    font-size: 25pt;
+    line-height: .5;
+    padding-bottom: 20px;
+    padding-top: 25px;
+    padding-left: 35px;
+    color: #800020;
+    font-family: 'Open Sans', sans-serif;
+  }
+h1 {
+    display: block;
+    padding: 7px;
+    padding-top: 15px;
+    font-size: 20px;
+     margin: auto;
+     text-align: center;
+     font-family: 'open sans';
+  }
+  a:link, a:visited {
+    color: white;
+    text-decoration: none;
+  }
+
+  a:hover {
+    background-color: ;
+    color: #800020;
+  }
+
+  a {
+    padding: 20px;
+  }
+</style>
+<header>
+  <section>
+  <h3>Katie Lynn Swiatek</h3>
+  <nav>
+    <a href="about.html">About</a>
+    <a href="examples.html">Work Samples</a>
+    <a href="contact.html">Contact</a>
+  </nav>
+  </section>
+</header>
+<body>
+<h1>Thank you for your message!</h1>
+<p>I have received your message and will be in touch shortly.</p>
+</body>
